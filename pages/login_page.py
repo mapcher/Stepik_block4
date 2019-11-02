aec5702461c18e25360ae92aa93d6b1260121e5c
nemoj <1c.shakhty@mail.ru>
2019-11-02 21:26:24 +0300

    method of getting page object changed

--- pages/login_page.py
+++ pages/login_page.py
@@ -8,10 +8,10 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        assert "login" in self.browser.current_url, "'login' not in current url"
+        assert "login" in self.browser.current_url, "'login' should be in current url"
 
     def should_be_login_form(self):
-        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form should be not present"
 
     def should_be_register_form(self):
-        assert self.is_element_present(*LoginPageLocators.REGISTRATION_FORM), "Register form is not presented"
+        assert self.is_element_present(*LoginPageLocators.REGISTRATION_FORM), "Register form should be present"
