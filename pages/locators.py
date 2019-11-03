6f558da372cb7a79e7ffd5bd0a9775b0c3c38db6
nemoj <1c.shakhty@mail.ru>
2019-11-03 22:45:17 +0300

    check of product name and price added

--- pages/locators.py
+++ pages/locators.py
@@ -6,3 +6,9 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTRATION_FORM = (By.CSS_SELECTOR, "#register_form")
+
+class ProductPageLocators():
+    ADD_TO_BASKET_BUTTON = (By.CSS_SELECTOR, "button.btn-add-to-basket")
+    PRODUCT_NAME = (By.CSS_SELECTOR, "div.product_main>h1")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, "p.price_color")
+    ALERT1 = (By.CSS_SELECTOR, "div.alertinner")
